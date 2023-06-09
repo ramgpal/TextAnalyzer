eba138c0592d18c957b8eb7100fe7bac712ca97e
ramgpal <ramgpal912@gmail.com>
2023-06-09 20:25:10 +0530

    Project Added

---
+++ index.js
@@ -0,0 +1,24 @@
+uppercase.addEventListener('click', ()=>{
+    console.log("transforming to UpperCase");
+    inpText.value = inpText.value.toUpperCase();
+})
+lowercase.addEventListener('click', ()=>{
+    console.log("transforming to LowerCase");
+    inpText.value = inpText.value.toLowerCase();
+})
+removeSpaces.addEventListener('click', ()=>{
+    inpText.value = inpText.value.replace(/\s+/g, " ").trim();
+})
+
+removeLines.addEventListener('click', ()=>{
+    inpText.value = inpText.value.replace(/\s+/g, "\n").trim();
+})
+
+inpText.addEventListener("input", () => {
+    console.log("changed");
+    var inputText = inpText.value;
+    charCount.innerText = inputText.length;
+    wordCount.innerText = inputText.trim().split(/\s+/).length-1;
+    sentenceCount.innerText = inputText.split(/[.?!]\s*/).length-1;
+});
+
